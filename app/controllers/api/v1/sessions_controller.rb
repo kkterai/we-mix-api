5063f5265001938b1c817b7eb2fb7c9aebbfc6ca
kkterai <kkterai@gmail.com>
2017-10-09 23:45:30 -0700

    build out login and signup

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,12 @@
+class Api::V1::SessionsController < ApplicationController
+
+    def login
+        user = User.find_by(email: params[:email])
+        if user && user.authenticate(params[:password])
+            render json: user
+        else
+            render json: { errors: { message: "Unable to find a user with that email or password" } }, status: 500
+        end
+    end
+
+end
---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,17 @@
+class Api::V1::UsersController < ApplicationController
+
+    def signup
+        user = User.new(user_params)
+        if user.save
+            render json: user
+        else
+            render json: { errors: user.errors.full_messages }, status: 500
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password, :email)
+    end
+end
