6f23c698c134ec03c0db8b328194a50901532c49
kkterai <kkterai@gmail.com>
2017-10-10 00:44:21 -0700

    built decode_token class method in auth

--- lib/auth.rb
+++ lib/auth.rb
@@ -5,11 +5,11 @@ class Auth
 
     def self.create_token(user_object)
         payload = { user: user_object }
-        JWT.encode(payload, 'myapp', 'HS256')
+        JWT.encode(payload, 'myapp', 'HS256') # ENV["Auth_secret"], ENV["Auth_algorithm"]
     end
 
-    def self.decode_token(object)
-
+    def self.decode_token(token)
+        JWT.decode(token, 'myapp', true, { algorithm: 'HS256' })
     end
 
 end
