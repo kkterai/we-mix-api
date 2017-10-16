f795503bf5e40671e1f7fe994f690250674e2168
kkterai <kkterai@gmail.com>
2017-10-16 15:41:05 -0700

    add initial seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+20.times do
+video = Video.create(
+    name: Faker::Lorem.word,
+    video_URL: Faker::Internet.url('example.com'), 
+    track_title: Faker::Ancient.primordial,
+    track_ID: Faker::Number.digit,
+    artist: Faker::Lorem.word,
+    artist_ID: Faker::Number.digit,
+    album: Faker::Lorem.word,
+    album_ID: Faker::Number.digit,
+    album_thumb: Faker::Lorem.word,
+    playlist_ID: Faker::Number.digit,
+    mood: Faker::Lorem.word).save
+end
