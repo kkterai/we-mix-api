b45765a680c3a77e1127962ac7f7d2786375f956
kkterai <kkterai@gmail.com>
2017-10-16 13:02:18 -0700

    fixed shoulda matchers and factory girl errors

--- spec/requests/videos_spec.rb
+++ spec/requests/videos_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe 'Videos API', type :request do
+RSpec.describe 'Videos API', type: :request do
 
     let!(:videos) { create_list(:video, 10) }
     let(:video_id) { videos.first.id }
 
     describe 'GET /api/v1/videos' do 
-        before { get '/api/v1/videos'}
+        before { get '/api/v1/videos' }
 
         it 'returns a status code 200' do
             expect(response).to have_http_status(200)
