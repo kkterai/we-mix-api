40e67a799777428f6cf4c588637d7e2c8c97efa7
kkterai <kkterai@gmail.com>
2017-11-14 13:06:39 -0800

    remove unnecessary comments and code

--- lib/auth.rb
+++ lib/auth.rb
@@ -1,11 +1,10 @@
 require 'JWT'
-require 'pry'
 
 class Auth
 
     def self.create_token(user_object)
         payload = { user: user_object }
-        JWT.encode(payload, 'myapp', 'HS256') # ENV["Auth_secret"], ENV["Auth_algorithm"]
+        JWT.encode(payload, 'myapp', 'HS256') 
     end
 
     def self.decode_token(token)
