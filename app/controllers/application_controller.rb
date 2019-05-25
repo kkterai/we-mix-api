de6953ffffd3416d1dd67af2a064a2b521abb529
kkterai <kkterai@gmail.com>
2019-05-25 12:49:16 -0700

    Add authorization actions which protect secure endpoints and add exception handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
+    
+    include JWTSessions::RailsAuthorization
+    rescue_from JWTSessions::Errors::Unauthorized, with: :not_authorized
 
 end
