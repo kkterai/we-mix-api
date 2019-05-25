9ff3db4a2514541296a40733e77c8abe5829bc81
kkterai <kkterai@gmail.com>
2019-05-25 16:00:50 -0700

    Give permission to refer to keys in the decoded body as both symbol and string

--- lib/auth.rb
+++ lib/auth.rb
@@ -8,7 +8,8 @@ class Auth
     end
 
     def self.decode_token(token)
-        JWT.decode(token, Rails.application.secrets.secret_key_base, true, { algorithm: 'HS256' })
+        body = JWT.decode(token, Rails.application.secrets.secret_key_base, true, { algorithm: 'HS256' })
+        HashWithIndifferentAccess.new(body)
     end
 
 end
