48b03ed96fdd2310b0695e3f04d1010e5183a407
kkterai <kkterai@gmail.com>
2019-05-25 15:50:28 -0700

    Use the secret_key_base as the secret

--- lib/auth.rb
+++ lib/auth.rb
@@ -4,11 +4,11 @@ class Auth
 
     def self.create_token(user_object)
         payload = { user: user_object }
-        JWT.encode(payload, 'myapp', 'HS256') 
+        JWT.encode(payload, Rails.application.secrets.secret_key_base, 'HS256') 
     end
 
     def self.decode_token(token)
-        JWT.decode(token, 'myapp', true, { algorithm: 'HS256' })
+        JWT.decode(token, Rails.application.secrets.secret_key_base, true, { algorithm: 'HS256' })
     end
 
 end
