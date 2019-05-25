770abeba934ec11b08173d61dda80c891f681a1e
kkterai <kkterai@gmail.com>
2019-05-25 12:42:28 -0700

    Add  to all user input fields for login

--- db/migrate/20171009204327_create_users.rb
+++ db/migrate/20171009204327_create_users.rb
@@ -3,9 +3,9 @@ class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     enable_extension 'pgcrypto' unless extension_enabled?('pgcrypto') 
     create_table :users, id: :uuid,  default: 'gen_random_uuid()' do |t|
-      t.string :username
-      t.string :password_digest
-      t.string :email
+      t.string :username, null: false
+      t.string :password_digest, null: false
+      t.string :email, null: false
       
       t.timestamps
     end
