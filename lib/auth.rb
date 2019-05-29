b1ef5ebca265cd03b58dd75fda5330181b5545aa
kkterai <kkterai@gmail.com>
2019-05-28 23:58:25 -0700

    Remove HashWithIndifferentAccess to restore original method output

--- lib/auth.rb
+++ lib/auth.rb
@@ -5,12 +5,10 @@ class Auth
     def self.create_token(user_object)
         payload = { user: user_object }
         JWT.encode(payload, ENV["AUTH_SECRET"], ENV["AUTH_ALGORITHM"]) 
-        binding.pry
     end
 
     def self.decode_token(token)
-        body = JWT.decode(token, ENV["AUTH_SECRET"], true, { algorithm: ENV["AUTH_ALGORITHM"] })
-        HashWithIndifferentAccess.new(body)
+        JWT.decode(token, ENV["AUTH_SECRET"], true, { algorithm: ENV["AUTH_ALGORITHM"] })
     end
 
 end
